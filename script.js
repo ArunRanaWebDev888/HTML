dc9b0ecc014504f26e907efb9b1457176aedf793
ArunRanaWebDev888 <aunrana8882072@gmail.com>
2024-11-21 21:07:18 +0530

    hi

---
+++ script.js
@@ -0,0 +1,48 @@
+//let friends =["aman","mohit","sachin","rohit","jula"];
+ 
+//for(let  friend of friends){
+  //  console.log(friend.toUpperCase());
+
+//}
+
+// it is the seconde programe
+
+/*let marks =[85,97,44,37,76,60];
+console.log(marks);
+let sum=0;
+let average=0;
+
+for(let mark of marks ){
+   
+    console.log(mark);
+    sum = sum +mark;
+   
+}
+
+console.log(sum);
+average =sum/marks.length;
+console.log(average);*/
+
+
+/*let items = [250,645,300,900,50];
+let offer = 10;
+let price=0;
+let i =0;
+
+for(let item of items){
+    console.log(i++,item);
+}
+
+for(let item of items){
+    
+price=  item/offer;
+finalprice=item-price;
+console.log('after appling offer  this is a final price =',finalprice);
+
+}*/
+
+
+let company = ['bloomberg','microsoft','uber','google','ibm',];
+console.log(company);
+company.splice(2,1,"ola");
+company.push("amazone")
